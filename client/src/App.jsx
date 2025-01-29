abcfbef6b094f424d3f3783af34c48eb94251005
Aidrin Peraira <aidrin.peraira@gmail.com>
2025-01-29 18:18:28 +0530

    got the front end setup to directly use page templates from material ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,12 @@
-import { useState } from 'react'
-import './App.css'
-import { Typography, Button, Container } from '@mui/material'
+import React, { useEffect } from 'react'
+import SignIn from './muiTemplates/sign-in/SignIn'
 
-function App() {
+const App = () => {
 
-  console.log('hello')
   return (
-    <>
-    <Container/>
-      <Typography>Hello Guys</Typography>
-      <Typography variant="h4" gutterBottom>
-          Welcome to My Material-UI App!
-        </Typography>
-        
-        <Typography variant="h6" gutterBottom>
-          This is a sample app demonstrating custom themes.
-        </Typography>
-        
-        {/* Buttons with custom primary and secondary colors */}
-        <Button color="primary" variant="contained" style={{ marginBottom: 20 }}>
-          Primary Button
-        </Button>
-        
-        <Button color="secondary" variant="contained">
-          Secondary Button
-        </Button>
-    <Container/>
-    </>
+    <div>
+      <SignIn/>
+    </div>
   )
 }
 
