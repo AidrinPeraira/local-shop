7680222130741746c9cd7d1bb209fde842753d9c
Aidrin Peraira <aidrin.peraira@gmail.com>
2025-02-13 09:49:11 +0530

    added the entire folder for frontend template

--- client/src/components/user-components/DashNav.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const DashNav = () => {
-  return (
-    <div>DashNav</div>
-  )
-}
--- client/src/pages/users/user-dash-items/UserDashHome.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const UserDashHome = () => {
-  return (
-    <div>UserDashHome</div>
-  )
-}
