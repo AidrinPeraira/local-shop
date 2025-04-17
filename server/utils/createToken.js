8c20079ef62b76d6f23fdae916619d49fbef08e4
Aidrin Peraira <aidrin.peraira@gmail.com>
2025-04-17 15:35:27 +0530

    Hosting Commit V2.3

--- server/utils/createToken.js
+++ server/utils/createToken.js
@@ -9,8 +9,8 @@ const generateToken = (res, userId) => {
     //this will set the value of the coookie to our jwt
     res.cookie('jwt', token, {
         httpOnly : true,
-        secure : process.env.NODE_ENV !== 'development',
-        sameSite : 'strict',
+        secure: process.env.NODE_ENV === 'production', 
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
         maxAge: 30*24*60*60*1000
     })
 
